17054ba5a08acd629b483cebc188c4a0931620b4
Chester Heng <chester.heng@gmail.com>
2023-04-20 16:17:51 +0800

    Challenge: Display all users to the browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import { users } from "./data/users";
 
 function App() {
+  const userItems = users.map(({ id, name }) => <li key={id}>{name}</li>);
   return (
-    <div>
-      JSX is cool!
-    </div>
+    <>
+      <h3>User names</h3>
+      <ul>{userItems}</ul>
+    </>
   );
 }
 
